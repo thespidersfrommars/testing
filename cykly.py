c3fa22732fdbd4d5bd2063146eb7e0f4d185b1ff
Houšková <houskaviktor2@gmail.com>
2025-10-31 11:21:07 +0100

    Add files via upload

---
+++ cykly.py
@@ -0,0 +1,55 @@
+#for i in range(0, 12, 2):
+#    print(i)
+
+#for i in range(6):
+#   print(i*2)
+
+#for i in range(0, 11):
+    #if i%2==0:
+       # print(i)
+
+#for i in range(5):
+    #for j in range(5):
+       # print(str(i) + ", " + str(j))
+
+#a = 0
+#while a == 0:
+   # x = input("napis cislo")
+   # try:
+    #    a = int(x)
+   # except:
+      #  a = 0
+
+
+for i in range(10):
+    print(i)
+    if i > 5:
+        break
+        #continue misto break kdyz ne mezi vsim
+    print("ahoj")
+
+
+a = 0
+while True:
+    x = input("napis cislo: ")
+    try:
+        a = int(x)
+        break
+    except:
+        pass
+    print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
